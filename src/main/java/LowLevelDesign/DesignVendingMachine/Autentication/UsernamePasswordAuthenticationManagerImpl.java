78c9d2a283cf9d6f70ee240150a3fca827f99151
Jhon Ever Diaz <jybeltran14@ucatolica.edu.co>
2024-03-08 14:30:00 -0500

    Add user

--- src/main/java/LowLevelDesign/DesignVendingMachine/Autentication/UsernamePasswordAuthenticationManagerImpl.java
+++ src/main/java/LowLevelDesign/DesignVendingMachine/Autentication/UsernamePasswordAuthenticationManagerImpl.java
@@ -1,7 +1,13 @@
 package LowLevelDesign.DesignVendingMachine.Autentication;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import LowLevelDesign.DesignVendingMachine.User;
 
@@ -10,8 +16,39 @@ public class UsernamePasswordAuthenticationManagerImpl implements Authentication
     private int intentosFallidos = 0;
 
     public UsernamePasswordAuthenticationManagerImpl() {
-        this.users = new ArrayList<>();
-        this.users.add(new User("admin", "12345"));
+        this.users = loadUsersFromFile();
+        if (users.isEmpty()) {
+            this.users.add(new User("admin", "12345"));
+            saveUsersToFile();
+        }
+    }
+
+    private List<User> loadUsersFromFile() {
+        List<User> loadedUsers = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File("users.txt"))) {
+            while (scanner.hasNextLine()) {
+                String[] userData = scanner.nextLine().split(",");
+                loadedUsers.add(new User(userData[0], userData[1]));
+            }
+        } catch (FileNotFoundException e) {
+            // Manejar la ausencia del archivo
+        }
+        return loadedUsers;
+    }
+
+    private void saveUsersToFile() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("users.txt"))) {
+            for (User user : users) {
+                writer.println(user.getUsername() + "," + user.getPassword());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addUser(String username, String password) {
+        users.add(new User(username, password));
+        saveUsersToFile();
     }
 
     @Override
